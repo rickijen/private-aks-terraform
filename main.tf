dbc212d607ac42c32f5fbb6732735defd810aa10
Rick Jen <rickijen@gmail.com>
2022-01-31 10:14:25 -0800

    updated post install for ubuntu

--- main.tf
+++ main.tf
@@ -294,8 +294,10 @@ sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo 'deb https://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y kubectl
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+# OSM
 OSM_VERSION=v0.11.1
 curl -sL "https://github.com/openservicemesh/osm/releases/download/$OSM_VERSION/osm-$OSM_VERSION-linux-amd64.tar.gz" | tar -vxzf -
 sudo mv ./linux-amd64/osm /usr/local/bin/osm
