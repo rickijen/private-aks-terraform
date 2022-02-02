cd8abd3e89f9eeeba9e63d7c341350d91f06fbf7
Rick Jen <rickijen@gmail.com>
2022-02-02 10:42:16 -0800

    updated azurerm version

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.94.0"
+      version = "=2.87.0"
     }
   }
 }
@@ -292,7 +292,6 @@ module "jumpbox" {
 /*
 sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo 'deb https://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 curl -fsSL https://get.docker.com -o get-docker.sh
