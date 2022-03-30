a30a39c7699470c262f7377f2a3bcaf813349110
Rick Jen <rickijen@gmail.com>
2022-03-30 14:49:08 -0700

    added azfw sku name and tier

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -10,6 +10,8 @@ resource "azurerm_firewall" "fw" {
   name                = var.fw_name
   location            = var.location
   resource_group_name = var.resource_group
+  sku_name            = "AZFW_VNet"
+  sku_tier            = "Standard"
 
   ip_configuration {
     name                 = "fw_ip_config"
