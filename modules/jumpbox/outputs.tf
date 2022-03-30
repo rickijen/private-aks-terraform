aa596fa42ee5d5e01bd672bb3637cc9c0ed047a5
Rick Jen <rickijen@gmail.com>
2022-03-30 08:49:51 -0700

    updated jumpbox outputs

--- modules/jumpbox/outputs.tf
+++ modules/jumpbox/outputs.tf
@@ -1,3 +1,4 @@
+/*
 output "jumpbox_ip" {
   description = "Jumpbox VM IP"
   #value       = azurerm_linux_virtual_machine.jumpbox.public_ip_address
@@ -8,6 +9,7 @@ output "jumpbox_username" {
   description = "Jumpbox VM username"
   value       = var.vm_user
 }
+*/
 
 #output "jumpbox_password" {
 #  description = "Jumpbox VM admin password"
