b476b68dc745f6a87d081d644dc3e3b3232c908a
Rick Jen <rickijen@gmail.com>
2022-03-30 08:53:55 -0700

    updated main.tf outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "ssh_command" {
-  value = "ssh ${module.jumpbox.jumpbox_username}@${module.jumpbox.jumpbox_ip}"
-}
-
 output "azurerm_log_analytics_workspace_name" {
   value = azurerm_log_analytics_workspace.default.name
 }
